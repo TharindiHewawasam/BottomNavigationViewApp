c7e7c051b7abacaadf9c6990f4751b29910f0ad7
TharindiHewawasam <malharit74@gmail.com>
2021-09-20 16:48:12 +0530

    add navigations to main activity

--- app/src/main/java/com/example/bottomnavigationviewapp/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigationviewapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.bottomnavigationviewapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, new DashboardFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull  MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.dashboardFragment:
+                    selectedFragment = new DashboardFragment();
+                    break;
+                case R.id.profileFragment:
+                    selectedFragment = new ProfileFragment();
+                    break;
+                case R.id.settingsFragment:
+                    selectedFragment = new SettingsFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, selectedFragment).commit();
+
+            return true;
+        }
+    };
 }
